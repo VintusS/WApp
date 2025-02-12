62014bebcc379ce8068755d45a794e094a24ef15
VintusS <dragomir.mindrescu@isa.utm.md>
2025-02-12 11:52:19 +0200

    Initial Commit

---
+++ WApp/WAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAppApp.swift
+//  WApp
+//
+//  Created by Dragomir Mindrescu on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
