fee2c237bf308fb4cf4209404209736b2a976ebd
VintusS <dragomir.mindrescu@isa.utm.md>
2025-02-12 12:18:21 +0200

    Add LocationManager

---
+++ WApp/Managers/LocationManager.swift
@@ -0,0 +1,37 @@
+//
+//  LocationManager.swift
+//  WApp
+//
+//  Created by Dragomir Mindrescu on 12.02.2025.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: LocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("Error getting location: ", error)
+        isLoading = false
+    }
+    
+}
