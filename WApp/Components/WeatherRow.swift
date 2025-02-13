9d2e1edf067063838d4452b24402246ad40ef3a2
VintusS <dragomir.mindrescu@isa.utm.md>
2025-02-13 09:57:31 +0200

    Weather view init

---
+++ WApp/Components/WeatherRow.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherRow.swift
+//  WApp
+//
+//  Created by Dragomir Mindrescu on 12.02.2025.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherRow()
+}
--- WApp/Views/WeatherView.swift
+++ WApp/Views/WeatherView.swift
@@ -11,7 +11,73 @@ struct WeatherView: View {
     var weather: ResponseBody
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold()
+                        .font(.title)
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        HStack {
+                            VStack(spacing: 20) {
+                                Image(systemName: "cloud.fill")
+                                    .font(.system(size: 40))
+                                
+                                Text(weather.weather[0].main)
+                            }
+                            
+                            Spacer()
+                            
+                            Text(weather.main.feels_like.roundDouble() + "º")
+                                .font(.system(size: 100))
+                                .fontWeight(.bold)
+                                .padding()
+                        }
+                        Spacer()
+                            .frame(height: 80)
+
+                        AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 350)
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        
+                        Spacer()
+                        
+                        
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                VStack {
+                    Spacer()
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        Text("")
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    .padding(.bottom, 20)
+                    .foregroundStyle(Color.blue.opacity(0.5))
+                    .background(.white)
+                    .cornerRadius(20, corners: [.topLeft, .topRight])
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color.blue.opacity(0.5))
+        .preferredColorScheme(.dark)
     }
 }
 
